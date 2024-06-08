d502c31dbe83946ad0c00f5c42b603bdd0435d81
ANKIT CHOUREY <153617714+Ankit907@users.noreply.github.com>
2024-06-08 22:22:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,97 @@
 # main.tf
-
 provider "aws" {
-  region = "ap-south-1" # Set your desired AWS region
+  region = "ap-south-1"
 }
 
-resource "aws_ecs_cluster" "my_cluster" {
-  name = "my-cluster"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_ecs_task_definition" "hello_world_task" {
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "allow_http" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
   family                   = "hello-world-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  network_mode             = "awsvpc"
-  
+
   container_definitions = jsonencode([
     {
-      name      = "hello-world-container"
-      image     = "ankit8770/hello-world-nodejs:latest"
-      cpu       = 256
-      memory    = 512
+      name  = "hello-world"
+      image = "${aws_ecr_repository.hello_world.repository_url}:latest"
       portMappings = [
         {
-          containerPort = 3000,
-          hostPort      = 3000,
-          protocol      = "tcp",
-        },
-      ],
-    },
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
   ])
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 }
 
-resource "aws_ecs_service" "hello_world_service" {
+resource "aws_ecr_repository" "hello_world" {
+  name = "hello-world-boss"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
+resource "aws_ecs_service" "hello_world" {
   name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.hello_world_task.arn
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.main.id] # Replace with your subnet ID
-    security_groups = [aws_security_group.allow_http.id]   # Replace with your security group ID
+    subnets          = [aws_subnet.main.id]
+    security_groups  = [aws_security_group.allow_http.id]
     assign_public_ip = true
   }
 }
