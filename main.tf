4bbe094cca285afb02692ff9214f22c8e461b67b
ANKIT CHOUREY <153617714+Ankit907@users.noreply.github.com>
2024-06-09 11:05:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
-# main.tf
 provider "aws" {
   region = "ap-south-1"
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main_vpc"
+  }
 }
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "main_subnet"
+  }
 }
 
 resource "aws_security_group" "allow_http" {
@@ -22,43 +29,27 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_ecs_cluster" "main" {
-  name = "hello-world-cluster"
-}
-
-resource "aws_ecs_task_definition" "hello_world" {
-  family                   = "hello-world-task"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "256"
-  memory                   = "512"
-
-  container_definitions = jsonencode([
-    {
-      name  = "hello-world"
-      image = "ankit8770/hello-world-nodejs:latest"
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-    }
-  ])
-
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  tags = {
+    Name = "allow_http"
+  }
 }
 
 resource "aws_ecr_repository" "hello_world" {
-  name = "hello-world-pearl"
+  name = "hello-world-pearl1"
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
@@ -82,6 +73,33 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   ]
 }
 
+resource "aws_ecs_cluster" "main" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "hello-world"
+      image = "637423558639.dkr.ecr.ap-south-1.amazonaws.com/hello-world-pearl:latest"
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+}
+
 resource "aws_ecs_service" "hello_world" {
   name            = "hello-world-service"
   cluster         = aws_ecs_cluster.main.id
@@ -94,4 +112,9 @@ resource "aws_ecs_service" "hello_world" {
     security_groups  = [aws_security_group.allow_http.id]
     assign_public_ip = true
   }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name       = aws_ecs_cluster.main.name
+  capacity_providers = ["FARGATE"]
 }
