889ee98ed4b5ce93d9c72a2f5109b5208d87b0c4
ANKIT CHOUREY <153617714+Ankit907@users.noreply.github.com>
2024-06-09 16:00:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24" 
 
   tags = {
     Name = "main_subnet"
